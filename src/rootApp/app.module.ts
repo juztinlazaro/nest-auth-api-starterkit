bfb3ea42a9db6d8662781f63dab913b7e5acf988
Justin Lazaro <juztinlazaro@gmail.com>
2019-03-31 19:45:43 +0800

    alias

--- src/rootApp/app.module.ts
+++ src/rootApp/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { ItemModule } from '@app/modules/items/items.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import config from '../configurations/keys';
-import { ItemModule } from '../items/items.module';
 
 @Module({
   imports: [MongooseModule.forRoot(config.mongoURI), ItemModule],
