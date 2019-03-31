b0dc9c2f0a01d92c918a8e25f1ceb8146a627de9
Justin Lazaro <juztinlazaro@gmail.com>
2019-03-31 19:31:24 +0800

    changes: folder structure

--- src/rootApp/app.module.ts
+++ src/rootApp/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import config from './configurations/keys';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemModule } from './items/items.module';
+
+import config from '../configurations/keys';
+import { ItemModule } from '../items/items.module';
 
 @Module({
   imports: [MongooseModule.forRoot(config.mongoURI), ItemModule],
