7da5dc552d0557821135b07be02fee719b560af9
Justin Lazaro <juztinlazaro@gmail.com>
2019-04-06 21:43:31 +0800

    changes: fix import of items

--- src/rootApp/app.module.ts
+++ src/rootApp/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { ItemModule } from '@app/modules/items/items.module';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import config from '../../configurations/keys';
+import { ItemSchema } from '@app/shared/schema/item.schema';
+
+import { ItemsController } from '@app/modules/items/items.controller';
+import { ItemsService } from '@app/modules/items//items.service';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoURI), ItemModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(config.mongoURI),
+    MongooseModule.forFeature([
+      {
+        name: 'Item',
+        schema: ItemSchema,
+      },
+    ]),
+  ],
+  controllers: [AppController, ItemsController],
+  providers: [AppService, ItemsService],
 })
 export class AppModule {}
