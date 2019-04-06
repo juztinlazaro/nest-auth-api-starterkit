9d3acd7ea538c9743ea8fda859fcd1cfccdd9ca4
Justin Lazaro <juztinlazaro@gmail.com>
2019-04-07 03:19:24 +0800

    changes: delete verify token when account isVerified

--- src/modules/verification-token/service/account-verification/account-verification.service.ts
+++ src/modules/verification-token/service/account-verification/account-verification.service.ts
@@ -3,20 +3,41 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { IVerificationTokenModel } from '@app/shared/model/verification-token.model';
+import { IAuthenticationModel } from '@root/src/shared/model/authentication.model';
 
 @Injectable()
 export class AccountVerificationService {
   constructor(
     @InjectModel('VerificationToken')
     private readonly verificationTokenModel: Model<IVerificationTokenModel>,
+    @InjectModel('Authentication')
+    private readonly authModel: Model<IAuthenticationModel>,
   ) {}
 
   checkVerificationToken(token: string): Promise<void> {
-    console.log('token', token);
     return this.verificationTokenModel
       .findOne({
         token,
       })
-      .then(res => console.log('res', res));
+      .then(res => {
+        console.log('res***', res);
+        this.verifyAccount(res.authId);
+      })
+      .catch(error => console.log('error****', error));
+  }
+
+  verifyAccount(authId: string): Promise<void> {
+    return this.authModel
+      .updateOne(
+        {
+          _id: authId,
+        },
+        { isVerified: true },
+      )
+      .then(() => this.deleteVerificationToken(authId));
+  }
+
+  deleteVerificationToken(authId: string): Promise<void> {
+    return this.verificationTokenModel.deleteOne({ authId });
   }
 }
